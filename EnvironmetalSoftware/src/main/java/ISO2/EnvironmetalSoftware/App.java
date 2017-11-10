cec9550d45f2d3b1d3b3d155f9afd4d6b9c8694f
marianESI <lelesmj@gmail.com>
2017-11-10 14:23:51 +0100

    tercero

--- EnvironmetalSoftware/src/main/java/ISO2/EnvironmetalSoftware/App.java
+++ EnvironmetalSoftware/src/main/java/ISO2/EnvironmetalSoftware/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Mari!, Albertojbgdfg" );
-        System.out.println( "marian moreno" );
+        System.out.println( "marian moreno679" );
     }
 }
