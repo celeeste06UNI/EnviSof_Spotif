6952311dc7eb41f4ed97cc22f6648a5c4406567a
celeeste06UNI <32294156+celeeste06UNI@users.noreply.github.com>
2017-11-29 10:25:33 +0100

    Añadida interfaz

---
+++ EnvironmetalSoftware/iteracion1/src/main/java/Presentacion/VentanaInicial.java
@@ -0,0 +1,43 @@
+package Presentacion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VentanaInicial {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaInicial window = new VentanaInicial();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaInicial() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
